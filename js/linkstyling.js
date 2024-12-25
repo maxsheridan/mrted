9725c81c14ebc0effeff07b254597208801ef6bf
maxsheridan <max@maxsheridan.com>
2024-12-26 00:51:46 +0200

    Update linkstyling.js

--- js/linkstyling.js
+++ js/linkstyling.js
@@ -1,1 +1,1 @@
-var currentPath=window.location.pathname;var navLinks=document.querySelectorAll(".links a");navLinks.forEach(function(t){if(t.getAttribute("href")===currentPath){t.classList.add("active")}})
+var currentPath=window.location.pathname;var navLinks=document.querySelectorAll(".bunch-of-links a");navLinks.forEach(function(t){if(t.getAttribute("href")===currentPath){t.classList.add("active")}})
